19ace13a73fb346db596723ebb516569919cecc2
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-03-18 14:15:55 -0400

    Set up picked number

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,12 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int pickedNumber;
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+    }
+}
