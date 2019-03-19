92fd12dbae1b73aa1e4a2f3388ac7c2b13286f8e
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-03-19 13:48:26 -0400

    Set up scanner and created a method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,12 +1,25 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
         int pickedNumber;
         pickedNumber = (int)(Math.random()*100);
-
         System.out.println(pickedNumber);
+
+        int guessedNumber;
+        guessedNumber = keyboard.nextInt();
+
+
+
+
+
     }
-}
+
+    public static String printNumberHigh(){System.out.println("You guessed too high");}
